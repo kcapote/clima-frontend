162e1186c3b0ef17190c7bfb8865e9beb41a1e91
Kenny Capote <kgcapoteb.alaya@bancochile.cl>
2019-07-24 15:59:29 -0400

    spinner de carga

--- src/components/climas/Climas.js
+++ src/components/climas/Climas.js
@@ -1,38 +1,42 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import Clima from './Clima';
 import axios  from 'axios';
+import Spinner from '../spinner/Spinner';
 
 const Climas = () => {
      
     const [ climas, setClimas ] = useState([]);
-
+    const [ cargando, setCargando ] = useState(false);
     useEffect(()=>{
         cargarClimas()
     },[]);
 
     const cargarClimas = async () => {
+        setCargando(true);
         let respuesta ='';
         try{
             respuesta = await axios.get('http://localhost:3001/climas') ;
         }catch(err){
             console.log(err)
         }
-        console.log(respuesta);
-            
-        const {climas} = respuesta.data;
-        
+        console.log(respuesta);            
+        const {climas} = respuesta.data;        
         setClimas(climas);
+        setCargando(false);
     };
-
-
-    return (
-        <Fragment>
-            <h1 className="mt-5">Climas</h1>
-            {climas.map( clima =>    
-                <Clima key={clima.horaFormateada} clima={clima}/>
-            )} 
-        </Fragment>
-    );
+    
+    if(cargando){
+        return <Spinner/>
+    }else{
+        return (
+            <Fragment>
+                <h1 className="mt-5">Climas</h1>
+                {climas.map( clima =>    
+                    <Clima key={clima.horaFormateada} clima={clima}/>
+                )}                
+            </Fragment>
+        );
+    }
 }
  
 export default Climas;
